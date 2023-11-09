86888da585fbd2a566028ffa10bdd806f7db001d
Jimzical <arishkumar2003@gmail.com>
2023-11-09 21:52:57 +0530

    added integrated code

--- app.py
+++ app.py
@@ -458,9 +458,67 @@ For more info check out the [Streamlit Docs](https://docs.streamlit.io/en/stable
 ''')
 st.divider()
 
+with st.expander("Integrated Version"):
+    st.code(
+        '''
+        import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+import pypdf
+
+st.set_page_config(page_title="Chat with the Uni data, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+openai.api_key = "Your API key here"
+st.title("Chat with your University Data, powered by LlamaIndex 💬🦙")
+
+
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about your University data"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Your University docs - hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert in analyzing student data of their partcular University, Assume all input prompts to be with respect to the input data, Don't answer anything apart from educational related prompt"))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+
+# st.text(index)
+
+# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
+
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)
+
+
+        '''
+    )
+
 st.write(" ")
 st.write(" ")
 st.write(" ")
 st.write(" ")
 st.markdown("<h2 style=\"color: gold; background: rgba(255, 215, 0, 0.1); border-radius: 10px; text-align: center;\">Thanks For Reading!</h2>", unsafe_allow_html=True)
-# st.markdown("<h2 style=\"color: gold; background: rgba(255, 215, 0, 0.1); border-radius: 10px; text-align: center; text-decoration: underline;\">Thanks For Reading!</h2>", unsafe_allow_html=True)
